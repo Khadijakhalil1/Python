5159a19f2ff6b643e41f878311865f9b2e586e99
Khadija230 <156936052+Khadijakhalil1@users.noreply.github.com>
2025-04-29 05:38:49 +0500

    count

---
+++ Practice/Lecture2/Length.py
@@ -0,0 +1,2 @@
+str = "I am a student Of BSCS at Islamiya College.BSCS stand for BS(Computer Science)"
+print("length of string is ",len(str))
---
+++ Practice/Lecture2/count.py
@@ -0,0 +1,2 @@
+str = "I am a student Of BSCS at Islamiya College.BSCS stand for BS(Computer Science)"
+print(str.count("BSCS"))
