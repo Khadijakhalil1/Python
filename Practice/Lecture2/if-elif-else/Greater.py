e6a8e6184f2d45b5bfea726002502f05c804435b
Khadija230 <156936052+Khadijakhalil1@users.noreply.github.com>
2025-04-29 06:07:21 +0500

    Greater

---
+++ Practice/Lecture2/if-elif-else/Greater.py
@@ -0,0 +1,13 @@
+N1 = int(input("num:"))
+print("1st Number is",N1)
+N2 = int(input("num:"))
+print("2nd Number is",N2)
+N3 = int(input("num:"))
+print("3rd Number is",N3)
+if(N1>=N2 and N1>=N3):
+    print("1st is Greater")
+elif(N2>=N1 and N2>=N3):
+    print("2nd is greater")
+else: 
+    print("3rd is greater")
+    
