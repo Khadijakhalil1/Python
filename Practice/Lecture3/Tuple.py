6eaac7499c16f0f5495a79da72517b24b88d0ced
Khadija230 <156936052+Khadijakhalil1@users.noreply.github.com>
2025-05-01 06:13:53 +0500

    grade

---
+++ Practice/Lecture3/Tuple.py
@@ -0,0 +1,11 @@
+tup = (1,5,2,3,10,4,1,2,2,)
+print(tup)
+print(tup.index(10))
+print(tup.count(2))
+
+grade = ("B","A","D","B","C","A","A","B","C","B",)
+print(grade.count("A"))
+#  Ascending order in Lists
+grade = ["B","A","D","B","C","A","A","B","C","B",]
+grade.sort()
+print(grade)
