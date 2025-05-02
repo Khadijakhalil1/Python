ee2f706c0638cd06dffbd211728050db1bb1abaa
Khadija230 <156936052+Khadijakhalil1@users.noreply.github.com>
2025-05-02 12:27:41 +0500

    Marks

---
+++ Practice/Lecture4/Quiz/quiz2.py
@@ -0,0 +1,11 @@
+Marks= {}
+k = int(input("Enter 1st:"))
+Marks.update({"math": k})
+
+k = int(input("Enter 2nd:"))
+Marks.update({"com": k})
+
+k = int(input("Enter 3rd:"))
+Marks.update({"phy": k}) 
+
+print(Marks)
