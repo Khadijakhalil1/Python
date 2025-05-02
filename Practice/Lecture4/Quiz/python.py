553b2740355067692fc7f79a375a7192b1300f16
Khadija230 <156936052+Khadijakhalil1@users.noreply.github.com>
2025-05-02 11:43:36 +0500

    Quiz1

---
+++ Practice/Lecture4/Quiz/python.py
@@ -0,0 +1,23 @@
+dic = {
+    "cat":"is an animal",
+    "table":
+    {
+        "a piece of furniture",
+        "list of facts and figures"
+    }
+}
+print(dic)
+sub={
+"python",
+"java",
+"c++",
+"java",
+"python",
+"javascript",
+"python",
+"java",
+"c++",
+"c",
+}
+print(sub)
+print("Length of class rooms",len(sub))
