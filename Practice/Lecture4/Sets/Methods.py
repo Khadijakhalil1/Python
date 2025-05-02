b2c26156aea0abfa5a6e935a11d3e796be93edf6
Khadija230 <156936052+Khadijakhalil1@users.noreply.github.com>
2025-05-02 05:51:48 +0500

    Apply methods

---
+++ Practice/Lecture4/Sets/Methods.py
@@ -0,0 +1,11 @@
+set = {"Hello","Students","Look","At","Here","33","44"}
+print(set)
+# apply methods
+set.add("addme")
+print(set)
+set.remove("addme")
+print(set)
+set.pop()
+print(set)
+set.clear()
+print(set)
