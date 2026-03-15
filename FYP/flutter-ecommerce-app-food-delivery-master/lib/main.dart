99212c35bdd1c42f2c7de8aed11fbc2af740e966
Khadija230 <156936052+Khadijakhalil1@users.noreply.github.com>
2026-03-15 15:32:08 +0500

    dart

---
+++ FYP/flutter-ecommerce-app-food-delivery-master/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter-ecommerce-app-food-delivery-master/home/main_food_page.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'food/popular_food_detail.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,//colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: PopularFoodDetail(),
+    );
+  }
+}
+
